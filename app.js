13a42ea1ac931ca2ff2d39c70b7fbc95ba88b846
Kyle Sallee <kyle.sallee@gmail.com>
2021-07-20 14:03:36 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Kyle!'));
+app.get('/', (req, res) => res.send('Hello Kyle and friends!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
